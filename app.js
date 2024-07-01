b17b26463988ecb21bd7a6d7f1e0774fa4f1fa62
ValerieNielson <Valerie.Nielson@365smartshop.com>
2024-07-01 16:56:34 -0400

    added a fun feature

--- app.js
+++ app.js
@@ -9,15 +9,39 @@ var pics = [
     "imgs/image8.jpg",
     "imgs/image9.jpg",
 ]
-
+var backgroundColors = [
+    "#ff6666",
+    "#ffb84d",
+    "#ffe680",
+    "#00b300",
+    "#47d1d1",
+    "#d9b3ff",
+    "#ffccff",
+    "#a3a375",
+    "#ecc6d9"
+]
 var btn = document.querySelector("button");
 var img = document.querySelector("img");
+var body = document.querySelector("body");
 var counter = 1;
+var resetCounter = 0;
+var imageCounter = 0;
 
 btn.addEventListener("click", function(){
     if (counter==9){
         counter = 0;
+        resetCounter = resetCounter + 1
     }
     img.src = pics[counter]
     counter = counter + 1
+
+    if (resetCounter > 3){
+        body.style.backgroundColor = backgroundColors[resetCounter];
+        btn.style.backgroundColor = backgroundColors[resetCounter];
+        if (resetCounter > 9) {
+            resetCounter = 0;
+        }
+    }
+
+
 });
